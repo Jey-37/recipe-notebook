e9a3614c1024b93e1fc6d7d47e50ea85de84bfdb
Vasil <70105028+Jey-37@users.noreply.github.com>
2023-09-28 12:01:59 +0300

    Needles @Repository annotations was removed

--- src/main/java/com/recipes/repo/IngredientRepository.java
+++ src/main/java/com/recipes/repo/IngredientRepository.java
@@ -2,8 +2,6 @@ package com.recipes.repo;
 
 import com.recipes.model.Ingredient;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface IngredientRepository extends CrudRepository<Ingredient, Integer> {
 }
--- src/main/java/com/recipes/repo/TagRepository.java
+++ src/main/java/com/recipes/repo/TagRepository.java
@@ -2,8 +2,6 @@ package com.recipes.repo;
 
 import com.recipes.model.Tag;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface TagRepository extends CrudRepository<Tag, Integer> {
 }
